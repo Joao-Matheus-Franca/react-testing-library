855ec9e57dc5741e815d039940efd4e521bc8f73
João Matheus Silva Franca <matheussfranca@live.com>
2022-09-19 17:29:50 -0300

    Concluído: Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testando a página de pokémons favoritos', () => {
+  test('Testando a página sem pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFoundMessage = screen.getByText(/no favorite pokemon found/i);
+    expect(noFoundMessage).toBeInTheDocument();
+  });
+  test('Testando a página com pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const favoritesList = localStorage.getItem('favoritePokemonIds');
+
+    if (favoritesList === null) {
+      const noFoundMessage = screen.getByText(/no favorite pokemon found/i);
+      expect(noFoundMessage).toBeInTheDocument();
+    }
+    if (favoritesList) {
+      const favoritesCards = screen.getAllByRole('link', { name: /more details/i });
+      expect(favoritesCards.length).toBe(favoritesList.length);
+    }
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import renderWithRouter from '../helpers/renderWithRouter';
+// import FavoritePokemons from '../pages/FavoritePokemons';
+
+// describe('', () => {
+//   test('', () => {
+//   });
+//   test('', () => {
+//   });
+//   test('', () => {
+//   });
+// });
