6debff7bb39ee48864efb8a09ea82ef17fbc0b91
João Matheus Silva Franca <matheussfranca@live.com>
2022-09-19 17:43:29 -0300

    Concluído: Requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,13 +1,19 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import renderWithRouter from '../helpers/renderWithRouter';
-// import FavoritePokemons from '../pages/FavoritePokemons';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import NotFound from '../pages/NotFound';
 
-// describe('', () => {
-//   test('', () => {
-//   });
-//   test('', () => {
-//   });
-//   test('', () => {
-//   });
-// });
+describe('Testando o componente Not Found', () => {
+  test('Testando o header do componente', () => {
+    renderWithRouter(<NotFound />);
+    const header = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(header).toBeInTheDocument();
+  });
+  test('testando a imagem do componente', () => {
+    renderWithRouter(<NotFound />);
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByRole('img');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe(src);
+  });
+});
